802df96e38392ab7266fa37a3ad0069059629a88
Giorgos Margaritis <gmargari@cs.uoi.gr>
2018-02-25 18:06:31 +0200

    Update script

--- scrape.py
+++ scrape.py
@@ -6,11 +6,10 @@ import sys
 import scrapy
 import random
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 min_sec = 1
-max_sec = 1
+max_sec = 5
 
 #==============================================================================
 # OasthArrivalsScraper ()
@@ -23,8 +22,8 @@ class OasthArrivalsScraper:
     def __init__(self):
         self.arrivals_option_name = 'Αφίξεις Γραμμών'
 #        self.bus_name = random.choice([ '02', '08', '10', '11', '14', '17', '27', '31' ]) # aristotelous apo egnatia
-#        self.bus_name = random.choice([ '03', '05', '06', '12', '33', '39', '58', '78' ]) # aristotelous apo tsimiski
-        self.bus_name = random.choice([ '02', '08', '10', '11', ]) # aristotelous apo egnatia
+        self.bus_name = random.choice([ '03', '05', '06', '12', '33', '39', '58', '78' ]) # aristotelous apo tsimiski
+#        self.bus_name = random.choice([ '02', '08', '10', '11', ]) # aristotelous apo egnatia
         self.bus_stop_name = 'ΠΛΑΤΕΙΑ ΑΡΙΣΤΟΤΕΛΟΥΣ'
         self.printing_interval = 1
 
@@ -66,57 +65,69 @@ class OasthArrivalsScraper:
     # menu_page_click_link ()
     #==========================================================================
     def menu_page_click_link(self, menu_option):
-        self.page_click_link('slideup', menu_option, '.')
+        elems = self.get_menu_options()
+        option = self.select_option_by_class_and_name(elems, "slideup", menu_option)
+        option.click()
 
     #==========================================================================
     # arrivals_page_click_bus ()
     #==========================================================================
     def arrivals_page_click_bus(self, bus_name):
-        self.page_click_link('sp1', bus_name, '..')
+        elems = self.get_menu_options()
+        option = self.select_option_by_class_and_name(elems, "sp1", bus_name)
+        option.click()
 
     #==========================================================================
     # arrivals_bus_page_click_stop ()
     #==========================================================================
     def arrivals_bus_page_click_stop(self, stop_name):
-        self.page_click_link('spt', stop_name, '.')
+        elems = self.get_menu_options()
+        option = self.select_option_by_class_and_name(elems, "spt", stop_name)
+        option.click()
 
     #==========================================================================
-    # page_click_link ()
+    # get_menu_options ()
     #==========================================================================
-    def page_click_link(self, class_name, elem_name, relative_elem_to_click_xpath):
-        xpath_template  = '//div[@class="cacher" and (not(@style) or not(contains(@style,"display: none")))]'
-        xpath_template += '/div[@class="holder"]'
-        xpath_template += '/div[contains(@class,"menu")]'
-        # select the first menu, e.g. in case of stop selection, there
-        # is the outward journey menu and return journey menu.
-        xpath_template += '[1]'
-        xpath_template += '/h3'
-        xpath_template += '//*[@class="%s" %s ]'
-        try:
-            extra_operand = 'and contains(text(), "%s")' % (elem_name)
-            xpath = xpath_template % (class_name, extra_operand)
-            elem = self.browser.find_element_by_xpath(xpath)
-        except (NoSuchElementException):
-            extra_operand = ''
-            xpath = xpath_template % (class_name, extra_operand)
-            elems = self.browser.find_elements_by_xpath(xpath)
-            print('No element "%s". Please check again the name is one of the following:' % (elem_name))
-            for elem in elems:
-                print(elem.get_attribute('innerHTML'))
-            sys.exit(1)
+    def get_menu_options(self):
+        xpath  = '//div[@class = "cacher" and (not(@style) or not(contains(@style, "display: none")))]'
+        xpath += '/div[@class = "holder"]'
+        xpath += '/div[contains(@class, "menu")]'
+        # select the first 'menu' div, e.g. in case of stop selection
+        # there is the outward journey menu and return journey menu.
+        xpath += '[1]'
+        xpath += '/h3'
 
-        elem_to_click = elem.find_element_by_xpath(relative_elem_to_click_xpath)
-        elem.click()
+        elems = self.browser.find_elements_by_xpath(xpath)
+        return elems
+
+    #==========================================================================
+    # select_option_by_class_and_name ()
+    #==========================================================================
+    def select_option_by_class_and_name(self, elems, class_name, elem_name):
+        xpath_template = './/*[@class = "%s" %s]'
+        extra_operand = 'and contains(text(), "%s")' % (elem_name)
+        xpath = xpath_template % (class_name, extra_operand)
+
+        for elem in elems:
+            try:
+                found_elem = elem.find_element_by_xpath(xpath)
+                return found_elem
+            except (NoSuchElementException):
+                continue
+
+        extra_operand = ''
+        xpath = xpath_template % (class_name, extra_operand)
+        print('No element "%s". Please check again the name is one of the following:' % (elem_name))
+        for elem in elems:
+            print(elem.find_element_by_xpath(xpath).get_attribute('innerHTML'))
 
     #==========================================================================
     # print_arrival_times ()
     #==========================================================================
     def print_arrival_times(self):
-
-        path = '//*[@id="hh"]/div[5]/div[4]/div/h3'
         arrivals_info = []
-        arrivals = self.browser.find_elements_by_xpath(path)
-        for arrival in arrivals:
+        elems = self.get_menu_options()
+        for arrival in elems:
             try:
                 bus_info_elem = arrival.find_element_by_class_name('sp1')
                 arrival_time_elem = arrival.find_element_by_class_name('sptime')
@@ -124,7 +135,9 @@ class OasthArrivalsScraper:
                 arrival_time = arrival_time_elem.get_attribute('innerHTML')
                 arrivals_info.append([bus_info, arrival_time])
             except (StaleElementReferenceException):
-                continue
+                print('Stale element exception, will retry in next interval')
+                time.sleep(random.randint(min_sec, max_sec))
+                return
 
         timestamp = int(time.time())
         for arrival_info in arrivals_info:
@@ -138,7 +151,7 @@ class OasthArrivalsScraper:
             print("%4s (%04s) %-40s : %3d" % (line_number, vehicle_number, line_name, arrival_time))
             print("%d,%s,%s,%d" % (timestamp, line_number, vehicle_number, arrival_time), file=self.output_file)
 
-        if len(arrivals):
+        if len(arrivals_info):
             print('')
 
         self.output_file.flush()

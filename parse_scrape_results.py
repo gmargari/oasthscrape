eb226bdbc304fa1c9c61732308a3a43a47040a70
Giorgos Margaritis <gmargari@cs.uoi.gr>
2018-02-26 23:03:15 +0200

    Update script

--- parse_scrape_results.py
+++ parse_scrape_results.py
@@ -29,10 +29,13 @@ with open(timetables_filename) as timetables_f:
             times_hh_mm = map(lambda x: x.split(":"), times)
             times_minutes = map(lambda hh_mm: int(hh_mm[0])*60 + int(hh_mm[1]), times_hh_mm)
             mapped = zip(times, times_minutes)
-            print("%s" % '\n'.join([ "%s => %s" % (m[0], m[1]) for m in mapped ]))
+#            print("%s" % '\n'.join([ "%s => %s" % (m[0], m[1]) for m in mapped ]))
 
-sys.exit(0)
+#sys.exit(0)
 
+prev_timestamp = 0
+vehicles = []
+prev_vehicles = []
 with open(arrivals_filename) as arrivals_f:
     lines = [ l.strip() for l in arrivals_f.readlines() ]
     for line in lines:
@@ -44,5 +47,28 @@ with open(arrivals_filename) as arrivals_f:
 
         date = datetime.utcfromtimestamp(timestamp)
         local_date = utc_to_local(date)
+
+        vehicles.append((line_number, vehicle_number))
+
+        if prev_timestamp and timestamp != prev_timestamp:
+            new_vehicles = []
+            for v in vehicles:
+                if v not in prev_vehicles:
+                    new_vehicles.append(v)
+
+            prev_vehicles = list(vehicles)
+            vehicles = []
+
+            if new_vehicles:
+                print('')
+                print('New vehicles:')
+                for v in new_vehicles:
+                    print("\t%3s - %s" % (v[0], v[1]))
+
+
+            print('-----------------------')
+
         print(local_date.strftime('%d-%m-%Y %H:%M:%S'), end='')
-        print(" %s/%s/%d" % (line_number, vehicle_number, arrival_time))
+        print("   %3s (%s) %d'" % (line_number, vehicle_number, arrival_time))
+
+        prev_timestamp = timestamp
